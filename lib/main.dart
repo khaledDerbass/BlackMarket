05676005a6ec4135d2aff384675ea6896c8a355b
k.shamout <k.shamout@t2.sa>
2022-10-10 18:08:12 +0300

    orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_storage/get_storage.dart';
 import 'generated/codegen_loader.g.dart';
 import 'src/app.dart';
@@ -10,11 +12,23 @@ void main() async{
   await EasyLocalization.ensureInitialized();
   await Firebase.initializeApp();
   await GetStorage.init();
-  runApp(EasyLocalization(
-      supportedLocales: const [Locale('en', 'US'), Locale('ar', 'JO')],
-      path: 'assets/languages',
-      fallbackLocale: const Locale('ar', 'JO'),
-      assetLoader: const CodegenLoader(),
-      child: const App()
-  ),);
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.deepPurpleAccent,
+  ));
+
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ],
+  ).then((val) {
+    runApp(EasyLocalization(
+        supportedLocales: const [Locale('en', 'US'), Locale('ar', 'JO')],
+        path: 'assets/languages',
+        fallbackLocale: const Locale('ar', 'JO'),
+        assetLoader: const CodegenLoader(),
+        child: const App()
+    ),);
+  });
+
 }
